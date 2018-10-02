c9160406a128a081e6db7d97e3c119feb9faf1b3
Peter Magnusson <magnusson.p@outlook.com>
2018-10-02 14:48:55 +0200

    Starting point

---
+++ WPF.MVVM.CaliburnMicro.DataGrid/Bootstrapper.cs
@@ -0,0 +1,24 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using WPF.MVVM.CaliburnMicro.DataGrid.ViewModels;
+
+namespace WPF.MVVM.CaliburnMicro.DataGrid
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            // Sets a viewmodel to start with
+            DisplayRootViewFor<ShellViewModel>();
+        }
+    }
+}
---
+++ WPF.MVVM.CaliburnMicro.DataGrid/ViewModels/ShellViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF.MVVM.CaliburnMicro.DataGrid.ViewModels
+{
+    public class ShellViewModel : Screen
+    {
+
+    }
+}
