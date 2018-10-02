a7283be5622b220cfc82e9845cb11f99b58d78a2
Peter Magnusson <magnusson.p@outlook.com>
2018-10-02 17:20:57 +0200

    Added alot of stuff

--- WPF.MVVM.CaliburnMicro.DataGrid/ViewModels/ShellViewModel.cs
+++ WPF.MVVM.CaliburnMicro.DataGrid/ViewModels/ShellViewModel.cs
@@ -1,4 +1,6 @@
 ﻿using Caliburn.Micro;
+using ClassLibrary;
+using ClassLibrary.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +11,22 @@ namespace WPF.MVVM.CaliburnMicro.DataGrid.ViewModels
 {
     public class ShellViewModel : Screen
     {
+        public BindableCollection<EmployeeModel> Employees1 { get; set; }
+        public BindableCollection<EmployeeModel> Employees2 { get; set; }
+        public BindableCollection<EmployeeModel> Employees3 { get; set; }
+        public BindableCollection<EmployeeModel> Employees4 { get; set; }
+
+        //public BindableCollection<SiteModel> Sites1 { get; set; }
+
+        public ShellViewModel()
+        {
+            Employees1 = new BindableCollection<EmployeeModel>(RandomData.GetEmployees());
+            Employees2 = new BindableCollection<EmployeeModel>(RandomData.GetEmployees());
+            Employees3 = new BindableCollection<EmployeeModel>(RandomData.GetEmployees());
+            Employees4 = new BindableCollection<EmployeeModel>(RandomData.GetEmployees());
+
+            //Sites1 = new BindableCollection<SiteModel>(RandomData.GetSites());
+        }
 
     }
 }
